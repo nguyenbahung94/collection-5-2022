6a13b739edb460fbdbdcdd81096c25d736066fba
nguyenbahung94 <mr.hungcity@gmail.com>
2022-11-02 10:27:34 +0700

    update code

--- app/src/main/java/com/example/buildexample82022/testci/AiHealthWebView.kt
+++ app/src/main/java/com/example/buildexample82022/testci/AiHealthWebView.kt
@@ -99,33 +99,3 @@ class AiHealthWebView : AppCompatActivity() {
         }
     }
 }
-
-class fmaxSize : Fragment() {
-     var _binding: AihWebViewBinding? = null
-     val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = AihWebViewBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        init()
-        event()
-    }
-
-    private fun init() {
-
-    }
-
-    private fun event() {
-
-    }
-
-}
